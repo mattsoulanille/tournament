33bb4e31b13753660d74f7d9dac59a2d0e75fa7c
Matthew Soulanille <matthew@soulanille.net>
2017-04-01 20:53:57 -0400

    added a button to shuffle the bracket

--- tournament/client/tournament.js
+++ tournament/client/tournament.js
@@ -3,6 +3,47 @@
 var socket = io();
 var room = window.location.pathname;
 
+
+
+
+function shuffleBracket() {
+    var ask = confirm("Are you sure you want to shuffle the bracket?");
+    if (ask) {
+	var toShuffle = []
+	bracketData.teams.forEach(function(item) {
+	    toShuffle.push(item[0]);
+	    toShuffle.push(item[1]);
+	});
+	
+	shuffle(toShuffle);
+	
+	var newTeams = [];
+	for (var i = 0; i < toShuffle.length; i+= 2) {
+	    var pairing = [toShuffle[i], toShuffle[i+1]];
+	    if (pairing[1] == undefined) {
+		pairing[1] = null;
+	    }
+	    newTeams.push(pairing);
+	}
+	bracketData.teams = newTeams;
+	setBracket(bracketData);
+    }
+    
+}
+
+
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items The array containing the items.
+ */
+// Copy pasta
+function shuffle(a) {
+    for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
+}
+
 /*
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.open('GET', '/bracket.json', true);
@@ -27,7 +68,9 @@ xmlhttp.onreadystatechange = function() {
 //xmlhttp.send(null);
 
 var bracket;
+var bracketData;
 let setBracket = function(data) {
+    bracketData = data;
     $('.jQBracket').remove(); // Disgusting code. Just Disgusting
     $(function() {
 	bracket = $('.bracket').bracket({
@@ -55,6 +98,7 @@ socket.on('test', function(data) {
 
 
 var onSave = function(data) {
+    bracketData = data;
     socket.emit('bracket', data)
 };
 
